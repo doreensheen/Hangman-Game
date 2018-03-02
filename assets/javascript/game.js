7271256b732839ea65ac9056d63d1916ebb91995
doreensheen <sheen.doreen@gmail.com>
2018-03-01 23:44:16 -0600

    generate random word, only allow alphabet key to be pressed, display guess to html page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,12 +6,37 @@ var counterWins = "(number of wins)";
 var counterGuesses = "(number of guesses left)";
     document.getElementById("guesses").innerHTML = counterGuesses;
 
-var counterLetters = "(array of letters guessed incorrectly)";
-    document.getElementById("letters").innerHTML = counterLetters;
-
-// Generate random word and display to html
-var listOfWords = ["cat", "dog", "pig"];
+    
+    // Generate random word and display to html
+    var listOfWords = ["cat", "dog", "pig"];
     var randomNumber = Math.floor(Math.random()*listOfWords.length);
     var randomWord = listOfWords[randomNumber];
     document.getElementById("mysteryWord").innerHTML = randomWord;
-    // console.log(randomNumber);
+    
+
+    // Possible 'key' guesses
+    var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    console.log(alphabet.length);
+
+    // Read key pressed and store info in var keyThatWasPressed
+    document.onkeyup = function(letter) {
+        var keyThatWasPressed = letter.key;
+    
+        // Determine whether key pressed is found in array alphabet; if key is not in alphabet, index = -1
+        index = alphabet.indexOf(keyThatWasPressed);
+        if (index === -1) {
+            alert("That is not a letter. Try again.");
+        }else {
+            // Display key pressed by player
+            var counterLetters = keyThatWasPressed;
+            document.getElementById("letters").innerHTML += ("  " + counterLetters + "  ");
+        }
+
+        // for (i=0; i < alphabet.length; i++) {
+        //     if (keyThatWasPressed === alphabet[i]) {
+        //         console.log("you pressed a letter");
+        //     } else {
+        //         console.log("that's not a letter");
+        //     };   
+        // };
+    };
