b606379b93df00a23e30a54f99039e0f8d303591
doreensheen <sheen.doreen@gmail.com>
2018-03-02 10:34:53 -0600

    break mystery word into one string per letter (dog is an array of 3 strings [d,o,g]

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,37 +6,46 @@ var counterWins = "(number of wins)";
 var counterGuesses = "(number of guesses left)";
     document.getElementById("guesses").innerHTML = counterGuesses;
 
-    
-    // Generate random word and display to html
-    var listOfWords = ["cat", "dog", "pig"];
-    var randomNumber = Math.floor(Math.random()*listOfWords.length);
-    var randomWord = listOfWords[randomNumber];
-    document.getElementById("mysteryWord").innerHTML = randomWord;
-    
 
-    // Possible 'key' guesses
-    var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    console.log(alphabet.length);
+// -----------------------------------------------------------------------------------------------------------
+// Display Mystery Word in Dashes:
 
-    // Read key pressed and store info in var keyThatWasPressed
-    document.onkeyup = function(letter) {
-        var keyThatWasPressed = letter.key;
+
+// Generate random word
+var listOfWords = ["cat", "dog", "pig"];
+var randomNumber = Math.floor(Math.random()*listOfWords.length);
+var randomWord = listOfWords[randomNumber];
+
+// Display random word to html letter by letter
+for (i = 0; i < randomWord.length; i++){
+    console.log(randomWord[i]);
+    var oneLetterOfWord = randomWord[i];
     
-        // Determine whether key pressed is found in array alphabet; if key is not in alphabet, index = -1
-        index = alphabet.indexOf(keyThatWasPressed);
-        if (index === -1) {
-            alert("That is not a letter. Try again.");
-        }else {
-            // Display key pressed by player
-            var counterLetters = keyThatWasPressed;
-            document.getElementById("letters").innerHTML += ("  " + counterLetters + "  ");
-        }
+    document.getElementById("mysteryWord").innerHTML += (oneLetterOfWord);
+}
 
-        // for (i=0; i < alphabet.length; i++) {
-        //     if (keyThatWasPressed === alphabet[i]) {
-        //         console.log("you pressed a letter");
-        //     } else {
-        //         console.log("that's not a letter");
-        //     };   
-        // };
+// -----------------------------------------------------------------------------------------------------------
+// Display Letters Guessed:
+// 
+// 
+// Possible 'key' guesses
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+console.log(alphabet.length);
+
+// Read key pressed and store info in var keyThatWasPressed
+document.onkeyup = function(letter) {
+    var keyThatWasPressed = letter.key;
+
+    // Determine whether key pressed is found in array alphabet; if key is not in alphabet, index = -1
+    index = alphabet.indexOf(keyThatWasPressed);
+    if (index === -1) {
+        alert("That is not a letter. Try again.");
+    }else {
+        // Display key pressed by player
+        var counterLetters = keyThatWasPressed;
+        document.getElementById("letters").innerHTML += ("  " + counterLetters + "  ");
     };
+};
+
+
+// -----------------------------------------------------------------------------------------------------------
