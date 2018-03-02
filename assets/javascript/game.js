a264234c57059197057264451e2112ce2474cc6c
doreensheen <sheen.doreen@gmail.com>
2018-03-02 16:25:16 -0600

    started working on replace dashes with correct letter when guessed, letters not appearing in correct order, connected displayLettersGuessed function to document.onkeyup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,30 @@
+// -----------------------------------------------------------------------------------------------------------
+// Display Letters Guessed:
+var myFunctions = {
 
+    displayLettersGuessed: function() {
+    
+        // Possible 'key' guesses
+        var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+        // console.log(alphabet.length);
+    
+        // Read key pressed and store info in var keyThatWasPressed
+        document.onkeyup = function(letter) {
+            var keyThatWasPressed = letter.key;
+            
+            // Determine whether key pressed is found in array alphabet; if key is not in alphabet, index = -1
+            index = alphabet.indexOf(keyThatWasPressed);
+            if (index === -1) {
+                alert("That is not a letter. Try again.");
+            }else {
+                // Display key pressed by player
+                var counterLetters = keyThatWasPressed;
+                document.getElementById("letters").innerHTML += ("  " + counterLetters + "  ");
+            };
+        };
+    },
+};
+// -------------------------------------------------------------------------------------------------------------
 // Generate counters and display correct count to html
 var counterWins = "(number of wins)";
     document.getElementById("wins").innerHTML = counterWins;
@@ -12,40 +38,47 @@ var counterGuesses = "(number of guesses left)";
 
 
 // Generate random word
-var listOfWords = ["cat", "dog", "pig"];
+var listOfWords = ["cac", "dod", "pip"];
 var randomNumber = Math.floor(Math.random()*listOfWords.length);
 var randomWord = listOfWords[randomNumber];
 
 // Display random word to html letter by letter
-for (i = 0; i < randomWord.length; i++){
-    console.log(randomWord[i]);
+for (i = 0; i < randomWord.length; i++) {
     var oneLetterOfWord = randomWord[i];
+    console.log(oneLetterOfWord);
+    var dash = "-";
     
-    document.getElementById("mysteryWord").innerHTML += (oneLetterOfWord);
-}
-
-// -----------------------------------------------------------------------------------------------------------
-// Display Letters Guessed:
-// 
-// 
-// Possible 'key' guesses
+    // document.getElementById("mysteryWord").innerHTML += (oneLetterOfWord);
+    dashes = document.getElementById("mysteryWord").innerHTML += (dash);
+    // dashes = document.getElementById("mysteryWord").innerHTML = [0,1,2];
+};
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-console.log(alphabet.length);
+var counterLettersCorrect = 0;
 
-// Read key pressed and store info in var keyThatWasPressed
 document.onkeyup = function(letter) {
-    var keyThatWasPressed = letter.key;
-
-    // Determine whether key pressed is found in array alphabet; if key is not in alphabet, index = -1
-    index = alphabet.indexOf(keyThatWasPressed);
-    if (index === -1) {
-        alert("That is not a letter. Try again.");
-    }else {
-        // Display key pressed by player
-        var counterLetters = keyThatWasPressed;
-        document.getElementById("letters").innerHTML += ("  " + counterLetters + "  ");
+    var keyPressed = letter.key;
+    console.log("keyPressed: " + keyPressed);
+    
+    for (j = 0; j < randomWord.length; j++) {
+        if (keyPressed === randomWord[j]) {
+            var nth = 0;
+            dashes = dashes.replace(/-/g, function(match, i, original){
+                nth++;
+                return (nth === (j+1)) ? (keyPressed) : match;
+            });
+            // var str = document.getElementById("mysteryWord").innerHTML;
+            // var rep = str.replace(dashes[j],keyPressed);
+            document.getElementById("mysteryWord").innerHTML = dashes;
+            console.log("dashes = " + dashes);
+            counterLettersCorrect = counterLettersCorrect + 1;
+            console.log("counterLettersCorrect: " + counterLettersCorrect);
+        } else {
+            myFunctions.displayLettersGuessed(); 
+        } 
     };
 };
+    
+
+    
 
 
-// -----------------------------------------------------------------------------------------------------------
