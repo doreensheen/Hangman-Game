8d71f976d3bfeabb75a739cd1b6f37ab18e46d88
doreensheen <sheen.doreen@gmail.com>
2018-03-03 13:38:51 -0600

    made Wins: and Number of Guesses Left: counters active

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@ var myFunctions = {
         // Possible 'key' guesses
         var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
         // console.log(alphabet.length);
-    
+        
         // Read key pressed and store info in var keyThatWasPressed
         document.onkeyup = function(letter) {
             var keyThatWasPressed = letter.key;
@@ -24,18 +24,11 @@ var myFunctions = {
         };
     },
 };
-// -------------------------------------------------------------------------------------------------------------
-// Generate counters and display correct count to html
-var counterWins = "(number of wins)";
-    document.getElementById("wins").innerHTML = counterWins;
-
-var counterGuesses = "(number of guesses left)";
-    document.getElementById("guesses").innerHTML = counterGuesses;
-
 
 // -----------------------------------------------------------------------------------------------------------
 // Display Mystery Word in Dashes:
 
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 // Generate random word
 var listOfWords = ["cac", "dod", "pip"];
@@ -52,14 +45,20 @@ for (i = 0; i < randomWord.length; i++) {
     dashes = document.getElementById("mysteryWord").innerHTML += (dash);
     // dashes = document.getElementById("mysteryWord").innerHTML = [0,1,2];
 };
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+
+
 var counterLettersCorrect = 0;
+var counterGuessesLeft = 10;
+var counterWordsCorrect = 0;
 
 document.onkeyup = function(letter) {
     var keyPressed = letter.key;
     console.log("keyPressed: " + keyPressed);
     
     for (j = 0; j < randomWord.length; j++) {
+        // if key pressed is a correct letter...
+        // 1. display letter in place of dash under "Current Word"
         if (keyPressed === randomWord[j]) {
             var nth = 0;
             dashes = dashes.replace(/-/g, function(match, i, original){
@@ -69,15 +68,34 @@ document.onkeyup = function(letter) {
             // var str = document.getElementById("mysteryWord").innerHTML;
             // var rep = str.replace(dashes[j],keyPressed);
             document.getElementById("mysteryWord").innerHTML = dashes;
-            console.log("dashes = " + dashes);
+            
+            // 2. increase Number of Letters Guessed by one
             counterLettersCorrect = counterLettersCorrect + 1;
+            
+            // console.log lines
+            console.log("dashes = " + dashes);
             console.log("counterLettersCorrect: " + counterLettersCorrect);
+            
+            // if key pressed is NOT a correct letter...
         } else {
+            // 1. display in "Letters Already Guessed"
             myFunctions.displayLettersGuessed(); 
+            
+            // 2. decrease Number of Guesses Left by one
+            counterGuessesLeft = counterGuessesLeft - 1;
+            
         } 
     };
 };
-    
+
+// -------------------------------------------------------------------------------------------------------------
+// Generate counters and display correct count to html
+
+var counterGuesses = counterGuessesLeft;
+document.getElementById("guesses").innerHTML = counterGuesses;
+
+var counterWins = counterWordsCorrect;
+    document.getElementById("wins").innerHTML = counterWins;
 
     
 
