8d6f41152845545ae37fbb291e2690f6b852dfc8
doreensheen <sheen.doreen@gmail.com>
2018-03-03 17:04:52 -0600

    correctly guessed letter finally populates in correct location

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,101 +1,88 @@
 // -----------------------------------------------------------------------------------------------------------
-// Display Letters Guessed:
-var myFunctions = {
+var myFunctions = {   
 
-    displayLettersGuessed: function() {
+    replaceAt: function(str,index,chr) {
+        if(index > str.length-1) return str;
+        return str.substr(0,index) + chr + str.substr(index+1);
+    },
+
+    displayCorrectLetter: function(keyPressed) {
+        // if key pressed is a correct letter...
+        if (keyPressed === wordArray[j]) {
+            // 1. display letter in place of dash under "Current Word"
+            var removedElements = dashesArray.splice(j, 1, keyPressed);
+            dashes = dashesArray.join("");
+
+            document.getElementById("mysteryWord").innerHTML = dashes;
+        }
+    },
+    
+    // Display Letters Guessed:
+    displayLettersGuessed: function(keyPressed) {
     
         // Possible 'key' guesses
         var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-        // console.log(alphabet.length);
-        
-        // Read key pressed and store info in var keyThatWasPressed
-        document.onkeyup = function(letter) {
-            var keyThatWasPressed = letter.key;
-            
+                
             // Determine whether key pressed is found in array alphabet; if key is not in alphabet, index = -1
-            index = alphabet.indexOf(keyThatWasPressed);
+            index = alphabet.indexOf(keyPressed);
             if (index === -1) {
                 alert("That is not a letter. Try again.");
             }else {
                 // Display key pressed by player
-                var counterLetters = keyThatWasPressed;
+                var counterLetters = keyPressed;
                 document.getElementById("letters").innerHTML += ("  " + counterLetters + "  ");
             };
-        };
     },
-};
-
-// -----------------------------------------------------------------------------------------------------------
-// Display Mystery Word in Dashes:
-
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
-// Generate random word
-var listOfWords = ["cac", "dod", "pip"];
-var randomNumber = Math.floor(Math.random()*listOfWords.length);
-var randomWord = listOfWords[randomNumber];
-
-// Display random word to html letter by letter
-for (i = 0; i < randomWord.length; i++) {
-    var oneLetterOfWord = randomWord[i];
-    console.log(oneLetterOfWord);
-    var dash = "-";
-    
-    // document.getElementById("mysteryWord").innerHTML += (oneLetterOfWord);
-    dashes = document.getElementById("mysteryWord").innerHTML += (dash);
-    // dashes = document.getElementById("mysteryWord").innerHTML = [0,1,2];
-};
-
-
-
-var counterLettersCorrect = 0;
-var counterGuessesLeft = 10;
-var counterWordsCorrect = 0;
-
-document.onkeyup = function(letter) {
-    var keyPressed = letter.key;
-    console.log("keyPressed: " + keyPressed);
-    
-    for (j = 0; j < randomWord.length; j++) {
-        // if key pressed is a correct letter...
-        // 1. display letter in place of dash under "Current Word"
-        if (keyPressed === randomWord[j]) {
-            var nth = 0;
-            dashes = dashes.replace(/-/g, function(match, i, original){
-                nth++;
-                return (nth === (j+1)) ? (keyPressed) : match;
-            });
-            // var str = document.getElementById("mysteryWord").innerHTML;
-            // var rep = str.replace(dashes[j],keyPressed);
-            document.getElementById("mysteryWord").innerHTML = dashes;
-            
-            // 2. increase Number of Letters Guessed by one
-            counterLettersCorrect = counterLettersCorrect + 1;
-            
-            // console.log lines
-            console.log("dashes = " + dashes);
-            console.log("counterLettersCorrect: " + counterLettersCorrect);
-            
-            // if key pressed is NOT a correct letter...
-        } else {
-            // 1. display in "Letters Already Guessed"
-            myFunctions.displayLettersGuessed(); 
-            
-            // 2. decrease Number of Guesses Left by one
-            counterGuessesLeft = counterGuessesLeft - 1;
-            
-        } 
-    };
 };
 
 // -------------------------------------------------------------------------------------------------------------
 // Generate counters and display correct count to html
-
-var counterGuesses = counterGuessesLeft;
-document.getElementById("guesses").innerHTML = counterGuesses;
+var counterLettersCorrect = 0;
+var counterGuessesLeft = 10;
+var counterWordsCorrect = 0;
 
 var counterWins = counterWordsCorrect;
     document.getElementById("wins").innerHTML = counterWins;
+
+var counterGuesses = counterGuessesLeft;
+    document.getElementById("guesses").innerHTML = counterGuesses;
+
+// -----------------------------------------------------------------------------------------------------------
+// Display Mystery Word in Dashes:
+
+
+// Generate random word
+var listOfWords = ["mystery"];
+var randomNumber = Math.floor(Math.random()*listOfWords.length);
+var randomWord = listOfWords[randomNumber];
+var wordArray = randomWord.split("");
+
+// Display random word to html letter by letter
+for (i = 0; i < wordArray.length; i++) {
+    var oneLetterOfWord = wordArray[i];
+    console.log(oneLetterOfWord);
+    var dash = "-";
+    dashes = document.getElementById("mysteryWord").innerHTML += (dash);
+};
+
+var dashesArray = dashes.split("");
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+// -----------------------------------------------------------------------------------------------------------
+// Sequence of Actions when key Pressed
+document.onkeyup = function(letter) {
+    var keyPressed = letter.key;
+    console.log("keyPressed: " + keyPressed);
+    
+    for (j = 0; j < wordArray.length; j++) {
+
+        myFunctions.displayCorrectLetter(keyPressed);
+
+        myFunctions.displayLettersGuessed(keyPressed);
+ 
+    };
+};
+    
 
     
 
